2b8072dde66017c14b642644918f23e45bd1cabb
Anna Hostrawser <alhostrawser@yahoo.com>
2025-11-11 18:04:14 -0600

    Implemented step 3 model and step 4 ML pipeline

--- ml/model.py
+++ ml/model.py
@@ -2,6 +2,7 @@ import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
 # TODO: add necessary import
+from sklearn.ensemble import RandomForestClassifier
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -20,7 +21,10 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
     # TODO: implement the function
-    pass
+    model = RandomForestClassifier(random_state=42)
+    model.fit(X_train, y_train)
+    return model
+    #pass
 
 
 def compute_model_metrics(y, preds):
@@ -50,7 +54,7 @@ def inference(model, X):
 
     Inputs
     ------
-    model : ???
+    model : sklearn RandomForestClassifier
         Trained machine learning model.
     X : np.array
         Data used for prediction.
@@ -60,7 +64,8 @@ def inference(model, X):
         Predictions from the model.
     """
     # TODO: implement the function
-    pass
+    return model.predict(X)
+    #pass
 
 def save_model(model, path):
     """ Serializes model to a file.
@@ -73,12 +78,18 @@ def save_model(model, path):
         Path to save pickle file.
     """
     # TODO: implement the function
-    pass
+    with open(path, "wb") as file:
+        pickle.dump(model, file)
+    #pass
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
     # TODO: implement the function
-    pass
+    with open(path, "rb") as file:
+        model = pickle.load(file)
+        return model
+    #https://knowledge.udacity.com/questions/1067153
+   #pass
 
 
 def performance_on_categorical_slice(
@@ -121,8 +132,17 @@ def performance_on_categorical_slice(
     X_slice, y_slice, _, _ = process_data(
         # your code here
         # for input data, use data in column given as "column_name", with the slice_value 
+        #https://knowledge.udacity.com/questions/1061693
+        data[data[column_name] == slice_value],
+        categorical_features=categorical_features,
+        label=label,
+        training=False,
+        encoder=encoder,
+        lb=lb
         # use training = False
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    preds = inference(model, X_slice)
+    #None # your code here to get prediction on X_slice using the inference function
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
+
--- model_card_template.md
+++ model_card_template.md
@@ -3,16 +3,25 @@
 For additional information see the Model Card paper: https://arxiv.org/pdf/1810.03993.pdf
 
 ## Model Details
+Anna Hostrawser created the model. It is Random Forest Classifier using the default hyperparameters in scikit-learn 1.7.2.
 
 ## Intended Use
+This model is intended to be used in the context of this udacity course. This model should be used to predict if the income of individuals exceeds $50K/yr based on census data.
 
 ## Training Data
+The data was obtained from the UCI Machine Learning Repository (https://archive.ics.uci.edu/dataset/20/census+income). 
+
+The original data set has 48842 rows, and a 80-20 split was used to break this into a train and test set. 
 
 ## Evaluation Data
+A 80-20 split was used to break this into a train and test set.
 
 ## Metrics
 _Please include the metrics used and your model's performance on those metrics._
+The model was evaluated using F1 score. The value is 0.6790.
+Precision: 0.7362 | Recall: 0.6300 | F1: 0.6790
 
 ## Ethical Considerations
+
 
 ## Caveats and Recommendations
