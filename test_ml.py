683c43ab56c952990d7c2dcb491dbf7dc06537fd
Anna Hostrawser <alhostrawser@yahoo.com>
2025-11-14 21:10:37 -0600

    Step 4: Unit Test

--- test_ml.py
+++ test_ml.py
@@ -2,37 +2,55 @@ import pytest
 # TODO: add necessary import
 import pandas as pd
 
-#for test 2
-from ml.data import process_data, apply_label
-from ml.model import train_model, compute_model_metrics, load_model
-from train_model import data, train, test, X_train, y_train
+#test 1
+from ml.model import train_model
+from train_model import X_train, y_train
 from sklearn.ensemble import RandomForestClassifier 
 
+#test 2
+from fastapi.testclient import TestClient
+from main import app
+# Instantiate the testing client
+client = TestClient(app)
+
+#test 3
+import numpy as np
+from ml.model import compute_model_metrics
+
+
 # TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    """
-    # add description for the first test
-    """
-    # Your code here
-    #check that the size of the dataset is reasonable
-    #pass
-
-
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_ML_algorithm():
     """
     # test that the ML model uses the expected algorithm
     """
     # Your code here
+    #check that the size of the dataset is reasonable
     model = train_model(X_train, y_train)
     assert isinstance(model, RandomForestClassifier)
     #pass
 
 
+# TODO: implement the second test. Change the function name and input as needed
+def test_get_status_code():
+    """
+    # add description
+    """
+    # Your code here
+    r = client.get("/")
+    assert r.status_code == 200
+    #pass
+
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_compute_model_metrics():
     """
     # add description for the third test
     """
     # Your code here
-    pass
+    y_true = np.array([1, 0, 1, 1])
+    y_pred = np.array([1, 0, 0, 1])
+    precision, recall, fbeta = compute_model_metrics(y_true, y_pred)
+    assert 0 < precision <= 1
+    assert 0 < recall <=1
+    assert 0 < fbeta <=1
+
+    #pass
